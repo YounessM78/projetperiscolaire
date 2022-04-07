8d3f3b77ed45e0b76869801176b3883c7e288647
enChargement <101469609+enChargement@users.noreply.github.com>
2022-04-07 17:47:11 +0200

    Update Employes_cantine.java

--- Employes_cantine.java
+++ Employes_cantine.java
@@ -1,23 +1,30 @@
+package Model;
 
 import java.util.*;
 
 /**
  * 
  */
-public class Employes_cantine {
+public class Employes_cantine extends Employes{
 
     /**
      * Default constructor
      */
-    public Employes_cantine(String n, String t) {
-    	n = Nom;
-    	t= Tache;
+    public Employes_cantine(String n, String p, String t) {
+    	super(n,p);
+    	this.Tache = t;
+    }
+    
+    public static Vector<Employes_cantine> listEmployes_cantine = new Vector<Employes_cantine>();
+    
+    public static void addEmployes_cantine(String n,String p,String tache) {
+    	listEmployes_cantine.add(new Employes_cantine(n,p,tache));
+    	System.out.println(listEmployes_cantine.size()+"employé de cantine");
     }
 
     /**
      * 
      */
-    public String Nom;
     
     /**
      * 
